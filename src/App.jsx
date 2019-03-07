1638e4e6291b0557cc867d5445369b8c84fbe13b
Edd Mann <the@eddmann.com>
2019-03-07 19:30:35 +0000

    Switch to CRA and use React Hooks

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './Game.jsx';
-
-window.initFunWithFlags = (flags, attempts, output) => {
-    ReactDOM.render(<Game flags={flags} attempts={attempts} />, output);
-};
