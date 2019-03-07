fb3a452f883477b0a41843922c5462de50ab7ff0
Edd Mann <the@eddmann.com>
2019-03-07 19:32:34 +0000

    Updates

---
+++ precache-manifest.331e01f97238ab93624a3f4cde853589.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0c4b2de439b23476c05",
+    "url": "/fun-with-flags/static/js/runtime~main.d0c4b2de.js"
+  },
+  {
+    "revision": "8234c0c1baa09ac0acba",
+    "url": "/fun-with-flags/static/js/main.8234c0c1.chunk.js"
+  },
+  {
+    "revision": "353a521b69cac2c2e3bd",
+    "url": "/fun-with-flags/static/js/2.353a521b.chunk.js"
+  },
+  {
+    "revision": "42c4720a87ac016339f4c590091c77b6",
+    "url": "/fun-with-flags/index.html"
+  }
+];
